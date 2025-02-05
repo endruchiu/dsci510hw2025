4657e94deb67bb056255f5ebd3e845bdb228b5c7
endruchiu <chiuandr@usc.edu>
2025-02-04 19:34:58 -0800

    task1.py

--- HW3/task1.py
+++ HW3/task1.py
@@ -1,1 +1,6 @@
+#task 1 Check for panlindrome
+def check_panlindrome(word:str)-> bool:
+    word = word.lower()   # lower case the string
+    return word == word[::-1]  #reverse the string
+check_panlindrome("civic")
 
